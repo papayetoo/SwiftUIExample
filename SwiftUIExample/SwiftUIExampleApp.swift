d0c3d7d170da19de0c3b244c47ee4cba96bb6254
papayetoo <papayetoo@swallaby.com>
2023-07-11 20:40:00 +0900

    Initial Commit

---
+++ SwiftUIExample/SwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExampleApp.swift
+//  SwiftUIExample
+//
+//  Created by 최최광현 on 2023/07/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
